0037b874f8974f38a90f6cef72ada2005b554a96
Khairul Imam <ki65559@gmail.com>
2024-03-14 15:46:57 +0800

    dont git nb checkpoint

--- .ipynb_checkpoints/map_kab-checkpoint.py
+++
@@ -1,5 +0,0 @@
-from map_provinsi import * 
-from util import *
-
-kota_tps = map_kota_tps(provinsi)
-kab_indonesia = filter_kab_indonesia(kota_tps)
--- .ipynb_checkpoints/map_provinsi-checkpoint.py
+++
@@ -1,4 +0,0 @@
-from util import *
-
-provinsi = pemilu.req(pemilu.wilayah(0))
-kode_to_provinsi = {item.get('kode'): item.get('nama') for item in provinsi}
